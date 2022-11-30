e55241f26a8543b1002927f9c40e83d751cac292
Priya Varshney <98008826+Priya-Varshney08@users.noreply.github.com>
2022-11-30 18:56:31 +0530

    Sort 0 and 1 ( Time Complexity)
    Take as input N, a number. Take N more inputs to form an array. The array contains only 0 and 1. Sort the array in a single scan.
    
    Input Format
    Enter the size of the array N and input N more numbers and store in the array
    
    Constraints
    Output Format
    Display the sorted array
    
    Sample Input
    5
    1
    1
    1
    0
    0
    Sample Output
    0 0 1 1 1

---
+++ Assgn2/Sort0_1TimeComplexity.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Sort0_1TimeComplexity {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for(int i=0; i<n; i++){
+            a[i] = sc.nextInt();
+        }
+
+        for(int i=0; i<a.length-1; i++){
+            for(int j=0; j<a.length-i-1; j++){
+                if(a[j]>a[j+1]){
+                    int t = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = t;
+                }
+            }
+        }
+
+        for(int i=0; i<a.length; i++){
+            System.out.print(a[i] + " ");
+        }
+    }
+}
